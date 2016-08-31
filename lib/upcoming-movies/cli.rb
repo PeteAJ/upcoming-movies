e9c8c149adeec04ac1392e8a26f1399f4c1afdba
Peter Jacobson <github email address>
2016-08-30 21:33:46 -0400

    cli changes lib revisions

--- lib/upcoming-movies/cli.rb
+++ lib/upcoming-movies/cli.rb
@@ -1,7 +1,35 @@
 class UpcomingMovies::CLI
 
   def call
-    puts "Upcoming Movies"
+    list_movies
+    menu
+    goodbye
   end
 
+  def list_movies
+    puts "Upcoming Movies"
+    puts <<-DOC
+    1. movie1
+    2. movie2
+    DOC
+  end
+
+  def menu
+    puts "enter the number of the movie you would like to learn more about"
+    input = nil
+    while input != "exit"
+    input = gets.strip.downcase
+    case input
+    when "1"
+      puts "more info on 1..."
+    when "2"
+      puts "more info on 2..."
+    end
+  end
+
+def goodbye 
+  puts "see you tomorrow for updated information"
+end
+
+end
 end
