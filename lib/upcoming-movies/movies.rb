b955569ee7ee8d5d41717b2800248c1f2d76527c
Peter Jacobson <github email address>
2016-09-01 14:16:59 -0400

    return object with the scraped properties

--- lib/upcoming-movies/movies.rb
+++ lib/upcoming-movies/movies.rb
@@ -43,13 +43,16 @@ end
 
   def self.scrape_imdb
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/?ref_=nv_mv_cs_4"))
-    name = doc.search("td.overview-top h4 a").text
-    date = doc.search("h4.li_group a")[0].text
-    length = doc.search("time").text
-    genre = doc.search("td.overview-top p span:nth-child(1)")[0].text
-    outline = doc.search("tr td div.outline")[0].text
+    
+    movie = self.new
+    movie.name = doc.search("td.overview-top h4 a").text
+    movie.date = doc.search("h4.li_group a")[0].text
+    movie.length = doc.search("time").text
+    movie.genre = doc.search("td.overview-top p span:nth-child(1)")[0].text
+    movie.outline = doc.search("tr td div.outline")[0].text
+    movie
+   
 
-    binding.pry
   end
 
  
