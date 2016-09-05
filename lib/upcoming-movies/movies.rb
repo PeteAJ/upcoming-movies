bfe7aa0c354c6bbedb3ac39df985801d656944bc
Peter Jacobson <github email address>
2016-09-05 19:21:54 -0400

    movies initialize self.new in self.scrape

--- lib/upcoming-movies/movies.rb
+++ lib/upcoming-movies/movies.rb
@@ -6,14 +6,14 @@ class UpcomingMovies::Movies
 
   def initialize(attributes ={})
     @name = attriubtes[:name]
-    @date =
-    @length =
-    @genre =
-    @outline = 
+    @date = attriubtes[:date]
+    @length = attriubtes[:length]
+    @genre = attriubtes[:genre]
+    @outline = attriubtes[:outline]
     @@all << self
   end
 
-  def self.today
+  def self.list_movies
     #return instances of movies
      # puts <<-DOC
     #1. The Disappointments Room
@@ -22,7 +22,7 @@ class UpcomingMovies::Movies
 
     #scrape box office mojo for movie release data
     #UpcomingMovies::Movies.new.movies
-    self.scrape_movies
+    #self.scrape_movies
 end
 
 
@@ -37,23 +37,23 @@ end
 
 
   def self.scrape_imdb
-    binding.pry
+    
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/?ref_=nv_mv_cs_4"))
     list_of_movies = doc.search("div.list.detail div.list_item")
     list_of_movies.each do |data|
 
     self.new({
-      name: data.css("td.overview-top h4 a").text
-      movie.date = data.css("h4.li_group a").text
-      movie.length = data.css("time").text
-      movie.genre = data.css("td.overview-top p span:nth-child(1)").text
-      movie.outline = data.css("tr td div.outline").text
+      name: data.css("td.overview-top h4 a").text,
+      date: data.css("h4.li_group a").text,
+      length: data.css("time").text,
+      genre: data.css("td.overview-top p span:nth-child(1)").text,
+      outline: data.css("tr td div.outline").text
     })
     
-  
+  end
 
 
-    #ibinding.pry
+    #binding.pry
    
   end
 
