802dc3efdf0291cd5940e32e75a138cd8c82240a
Peter Jacobson <github email address>
2016-09-05 23:24:00 -0400

    fix cli exit message

--- lib/upcoming-movies/cli.rb
+++ lib/upcoming-movies/cli.rb
@@ -3,22 +3,19 @@ class UpcomingMovies::CLI
   def call
     list_movies
     menu
-    goodbye
   end
 
   def list_movies
-    puts "Upcoming Movies" 
     x = nil
+    puts "Upcoming Movies" 
     UpcomingMovies::Movies.all.each.with_index(x) do |movie, i|
        puts "#{i}. #{movie.name} comes out on #{movie.date}. It's #{movie.length} and a #{movie.genre} movie."
-   
     end
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "enter the number of the movie you would like to learn more about"
     input = gets.strip.downcase
     if input.to_i > 0 && input.to_i <= 7
       UpcomingMovies::Movies.all.each.with_index(1) do |movie, i|
@@ -27,14 +24,14 @@ class UpcomingMovies::CLI
   end
       elsif input == "list"
       list_movies
+    elsif input == "exit"
+      puts "see you next time for updated movie information"
     else
       puts "Not sure what you want here"
     end
     end
   end
 
-def goodbye 
-  puts "see you next time for updated movie information"
-end
+
 
 end
